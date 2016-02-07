28f12a3dc5833ab34be931fada24489fe342e02c
Roman Kulesha <kulesha.r@gmail.com>
2016-02-07 04:41:25 +0300

    rewrote log component to simplify

---
+++ Source/Log/Log.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+public enum LogFlag: Int {
+  case Error, Warning, Info, Debug, Verbose
+}
+
+public typealias LogHandler = ((lazyMessage: () -> String, flag: LogFlag) -> Bool)
+public typealias LogFormatter = ((rawMessage: String?, flag: LogFlag, function: String, file: String, line: UInt32) -> String)
+
+public class Log {
+  
+  // set which message should be print, by default print all
+  public class func setHandler(handler: LogHandler) {
+    OFLogSetHandler { handler(lazyMessage: $0, flag: LogFlag(rawValue: $1.rawValue)!) }
+  }
+  
+  public class func setFormatter(formatter: LogFormatter) {
+    OFLogSetFormatter { formatter(rawMessage: $0, flag: LogFlag(rawValue: $1.rawValue)!, function: $2, file: $3, line: $4) }
+  }
+  
+  public class func error<T>(value: T?, function: String = __FUNCTION__, file: String = __FILE__, line: UInt32 = __LINE__) {
+    let message: String? = (value == nil) ? nil : String(reflecting: value!)
+    OFLog(message, .Error, function, file, line)
+  }
+  
+  public class func warning<T>(value: T?, function: String = __FUNCTION__, file: String = __FILE__, line: UInt32 = __LINE__) {
+    let message: String? = (value == nil) ? nil : String(reflecting: value!)
+    OFLog(message, .Warning, function, file, line)
+  }
+  
+  public class func info<T>(value: T?, function: String = __FUNCTION__, file: String = __FILE__, line: UInt32 = __LINE__) {
+    let message: String? = (value == nil) ? nil : String(reflecting: value!)
+    OFLog(message, .Info, function, file, line)
+  }
+  
+  public class func debug<T>(value: T?, function: String = __FUNCTION__, file: String = __FILE__, line: UInt32 = __LINE__) {
+    let message: String? = (value == nil) ? nil : String(reflecting: value!)
+    OFLog(message, .Debug, function, file, line)
+  }
+  
+  public class func verbose<T>(value: T?, function: String = __FUNCTION__, file: String = __FILE__, line: UInt32 = __LINE__) {
+    let message: String? = (value == nil) ? nil : String(reflecting: value!)
+    OFLog(message, .Verbose, function, file, line)
+  }
+  
+}
--- Source/Log/OFLog.m
+++ Source/Log/OFLog.m
@@ -1,97 +1,51 @@
 #import "OFLog.h"
 
-@implementation OFLogger
+static OFLogHandler _handler = nil;
+static OFLogFormatter _formatter = nil;
 
-+ (instancetype)consoleLoggerWithLogLevel:(OFLogLevel)logLevel {
-  return [[OFLogger alloc] initWithLogLevel:logLevel printer:^(OFLogFlag flag, NSString * _Nonnull message) {
-    printf("%s\n", [message UTF8String]);
-  }];
+void OFLogSetHandler(OFLogHandler handler) {
+  _handler = [handler copy];
 }
 
-- (instancetype)initWithLogLevel:(OFLogLevel)logLevel printer:(OFLogPrinter)printer formatter:(_Nullable OFLogFormatter)formatter {
-  self = [super init];
-  if (self) {
-    self.logLevel = logLevel;
-    self.printer = printer;
-    self.formatter = formatter;
-  }
-  return self;
+void OFLogSetFormatter(OFLogFormatter formatter) {
+  _formatter = [formatter copy];
 }
 
-- (instancetype)initWithLogLevel:(OFLogLevel)logLevel printer:(OFLogPrinter)printer {
-  return [self initWithLogLevel:logLevel printer:printer formatter:nil];
-}
-
-- (void)setFormatter:(OFLogFormatter)formatter {
-  if (formatter) {
-    _formatter = [formatter copy];
-  } else {
-    // set default formatter
-    NSDateFormatter *dateFormatter = [NSDateFormatter new];
-    dateFormatter.dateFormat = [NSDateFormatter dateFormatFromTemplate:@"ddMMyyyy HHmmssSSS" options:0 locale:nil];
-    _formatter = [^NSString * _Nonnull(id  _Nullable object, OFLogFlag flag, NSString * _Nonnull function, NSString * _Nonnull file, unsigned int line) {
-      NSString *currentTime = [dateFormatter stringFromDate:[NSDate date]];
-      NSString *flagString;
-      switch (flag) {
-        case OFLogFlagError:
-          flagString = @"ERROR";
-          break;
-        case OFLogFlagWarning:
-          flagString = @"WARNING";
-          break;
-        case OFLogFlagDebug:
-          flagString = @"DEBUG";
-          break;
-        case OFLogFlagInfo:
-          flagString = @"INFO";
-          break;
-        case OFLogFlagVerbose:
-          flagString = @"VERBOSE";
-          break;
+void OFLog(NSString * _Nullable message, OFLogFlag flag, NSString *function, NSString *file, unsigned int line) {
+  __block NSString *formattedMessage = nil;
+  OFLogLazyMessage lazyFormattedMessage = ^NSString * _Nonnull {
+    if (!formattedMessage) {
+      if (_formatter) {
+        formattedMessage = _formatter(message, flag, function, file, line);
+      } else {
+        NSDateFormatter *dateFormatter = [NSDateFormatter new];
+        dateFormatter.dateFormat = [NSDateFormatter dateFormatFromTemplate:@"ddMMyyyy HHmmssSSS" options:0 locale:nil];
+        NSString *timeString = [dateFormatter stringFromDate:[NSDate date]];
+        NSString *flagString;
+        switch (flag) {
+          case OFLogFlagError:
+            flagString = @"ERROR";
+            break;
+          case OFLogFlagWarning:
+            flagString = @"WARNING";
+            break;
+          case OFLogFlagDebug:
+            flagString = @"DEBUG";
+            break;
+          case OFLogFlagInfo:
+            flagString = @"INFO";
+            break;
+          case OFLogFlagVerbose:
+            flagString = @"VERBOSE";
+            break;
+        }
+        NSString *filename = [file lastPathComponent];
+        formattedMessage = [NSString stringWithFormat:@"%@ [%@] %@:%d (%@): %@", timeString, flagString, filename, line, function, message];
       }
-      NSString *fileName = [file lastPathComponent];
-      NSString *message = [object debugDescription];
-      return [NSString stringWithFormat:@"%@ [%@] %@:%d (%@): %@", currentTime, flagString, fileName, line, function, message];
-    } copy];
+    }
+    return formattedMessage;
+  };
+  if (_handler && _handler(lazyFormattedMessage, flag)) {
+    printf("%s\n", [lazyFormattedMessage() UTF8String]);
   }
 }
-
-- (void)log:(id)object withFlag:(OFLogFlag)flag function:(const char *)function file:(const char *)file line:(unsigned int)line {
-  if ((NSInteger)flag <= (NSInteger)self.logLevel) {
-    NSString *message = self.formatter(object, flag, [NSString stringWithUTF8String:function], [NSString stringWithUTF8String:file], line);
-    self.printer(flag, message);
-  }
-}
-
-@end
-
-static NSMutableArray *loggers = nil;
-
-@implementation OFLog
-
-+ (void)initialize {
-  static dispatch_once_t once;
-  dispatch_once(&once, ^{
-    loggers = [NSMutableArray new];
-  });
-}
-
-+ (void)registerLogger:(OFLogger *)logger {
-  [loggers addObject:logger];
-}
-
-+ (void)removeLogger:(OFLogger *)logger {
-  [loggers removeObject:logger];
-}
-
-+ (NSArray<OFLogger *> *)loggers {
-  return [loggers copy];
-}
-
-+ (void)log:(id)object withFlag:(OFLogFlag)flag function:(const char *)function file:(const char *)file line:(unsigned int)line {
-  for (OFLogger *logger in loggers) {
-    [logger log:object withFlag:flag function:function file:file line:line];
-  }
-}
-
-@end
