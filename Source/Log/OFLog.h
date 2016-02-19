fe7d7cfdf74649c07dcef6aafd3150bdf21ab6e3
Roman Kulesha <kulesha.r@gmail.com>
2016-02-19 13:34:29 +0300

    rename flag to level in logger

--- Source/Log/OFLog.h
+++ Source/Log/OFLog.h
@@ -2,31 +2,31 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-typedef NS_ENUM(NSInteger, OFLogFlag)
+typedef NS_ENUM(NSInteger, OFLogLevel)
 {
-  OFLogFlagError,
-  OFLogFlagWarning,
-  OFLogFlagInfo,
-  OFLogFlagDebug,
-  OFLogFlagVerbose
+  OFLogLevelError,
+  OFLogLevelWarning,
+  OFLogLevelInfo,
+  OFLogLevelDebug,
+  OFLogLevelVerbose
 };
 
 typedef NSString * _Nonnull (^OFLogLazyMessage)(void);
-typedef BOOL (^OFLogHandler)(OFLogLazyMessage lazyFormattedMessage, NSString * _Nullable rawMessage, OFLogFlag flag, NSString *function, NSString *file, unsigned int line);
+typedef BOOL (^OFLogHandler)(OFLogLazyMessage lazyFormattedMessage, NSString * _Nullable rawMessage, OFLogLevel level, NSString *function, NSString *file, unsigned int line);
 
-FOUNDATION_EXPORT void OFLog(NSString * _Nullable message, OFLogFlag flag, NSString *function, NSString *file, unsigned int line);
-FOUNDATION_EXPORT void OFLogSetHandler(OFLogHandler _Nullable handler); // set which message should be print in console, default handler return YES in DEBUG and NO otherwise
+FOUNDATION_EXPORT void OFLog(NSString * _Nullable message, OFLogLevel level, NSString *function, NSString *file, unsigned int line);
+FOUNDATION_EXPORT void OFLogSetHandler(OFLogHandler _Nullable handler); // log handler, decide which message should print, by default print all in DEBUG and nothing otherwise
 
-#define OFLogError(format, ...) OFLog([NSString stringWithFormat:format,##__VA_ARGS__], OFLogFlagError, __func__, __FILE__, __LINE__)
-#define OFLogWarning(format,...) OFLog([NSString stringWithFormat:format,##__VA_ARGS__], OFLogFlagWarning, __func__, __FILE__, __LINE__)
-#define OFLogInfo(format, ...) OFLog([NSString stringWithFormat:format,##__VA_ARGS__], OFLogFlagInfo, __func__, __FILE__, __LINE__)
-#define OFLogDebug(format, ...) OFLog([NSString stringWithFormat:format,##__VA_ARGS__], OFLogFlagDebug, __func__, __FILE__, __LINE__)
-#define OFLogVerbose(format, ...) OFLog([NSString stringWithFormat:format,##__VA_ARGS__], OFLogFlagVerbose, __func__, __FILE__, __LINE__)
+#define OFLogError(format, ...) OFLog([NSString stringWithFormat:format,##__VA_ARGS__], OFLogLevelError, __func__, __FILE__, __LINE__)
+#define OFLogWarning(format,...) OFLog([NSString stringWithFormat:format,##__VA_ARGS__], OFLogLevelWarning, __func__, __FILE__, __LINE__)
+#define OFLogInfo(format, ...) OFLog([NSString stringWithFormat:format,##__VA_ARGS__], OFLogLevelInfo, __func__, __FILE__, __LINE__)
+#define OFLogDebug(format, ...) OFLog([NSString stringWithFormat:format,##__VA_ARGS__], OFLogLevelDebug, __func__, __FILE__, __LINE__)
+#define OFLogVerbose(format, ...) OFLog([NSString stringWithFormat:format,##__VA_ARGS__], OFLogLevelVerbose, __func__, __FILE__, __LINE__)
 
-#define OFLogErrorObject(object) OFLog([object debugDescription], OFLogFlagError, __func__, __FILE__, __LINE__)
-#define OFLogWarningObject(object) OFLog([object debugDescription], OFLogFlagWarning, __func__, __FILE__, __LINE__)
-#define OFLogInfoObject(object) OFLog([object debugDescription], OFLogFlagInfo, __func__, __FILE__, __LINE__)
-#define OFLogDebugObject(object) OFLog([object debugDescription], OFLogFlagDebug, __func__, __FILE__, __LINE__)
-#define OFLogVerboseObject(object) OFLog([object debugDescription], OFLogFlagVerbose, __func__, __FILE__, __LINE__)
+#define OFLogErrorObject(object) OFLog([object description], OFLogLevelError, __func__, __FILE__, __LINE__)
+#define OFLogWarningObject(object) OFLog([object description], OFLogLevelWarning, __func__, __FILE__, __LINE__)
+#define OFLogInfoObject(object) OFLog([object description], OFLogLevelInfo, __func__, __FILE__, __LINE__)
+#define OFLogDebugObject(object) OFLog([object description], OFLogLevelDebug, __func__, __FILE__, __LINE__)
+#define OFLogVerboseObject(object) OFLog([object description], OFLogLevelVerbose, __func__, __FILE__, __LINE__)
 
 NS_ASSUME_NONNULL_END
--- Source/Log/OFLog.m
+++ Source/Log/OFLog.m
@@ -11,37 +11,36 @@ void OFLogSetHandler(OFLogHandler handler) {
   _handler = [handler copy];
 }
 
-void OFLog(NSString * _Nullable message, OFLogFlag flag, NSString *function, NSString *file, unsigned int line) {
+void OFLog(NSString * _Nullable message, OFLogLevel level, NSString *function, NSString *file, unsigned int line) {
   __block NSString *formattedMessage = nil;
   OFLogLazyMessage lazyFormattedMessage = ^NSString * _Nonnull {
     if (!formattedMessage) {
       NSDateFormatter *dateFormatter = [NSDateFormatter new];
       dateFormatter.dateFormat = [NSDateFormatter dateFormatFromTemplate:@"ddMMyyyy HHmmssSSS" options:0 locale:nil];
       NSString *timeString = [dateFormatter stringFromDate:[NSDate date]];
-      NSString *flagString;
-      switch (flag) {
-        case OFLogFlagError:
-          flagString = @"ERROR";
+      NSString *levelString;
+      switch (level) {
+        case OFLogLevelError:
+          levelString = @"ERROR";
           break;
-        case OFLogFlagWarning:
-          flagString = @"WARNING";
+        case OFLogLevelWarning:
+          levelString = @"WARNING";
           break;
-        case OFLogFlagDebug:
-          flagString = @"DEBUG";
+        case OFLogLevelDebug:
+          levelString = @"DEBUG";
           break;
-        case OFLogFlagInfo:
-          flagString = @"INFO";
+        case OFLogLevelInfo:
+          levelString = @"INFO";
           break;
-        case OFLogFlagVerbose:
-          flagString = @"VERBOSE";
-          break;
+        case OFLogLevelVerbose:
+          levelString = @"VERBOSE";
       }
       NSString *filename = [file lastPathComponent];
-      formattedMessage = [NSString stringWithFormat:@"%@ [%@] %@:%d (%@): %@", timeString, flagString, filename, line, function, message];
+      formattedMessage = [NSString stringWithFormat:@"%@ [%@] %@:%d (%@): %@", timeString, levelString, filename, line, function, message];
     }
     return formattedMessage;
   };
-  if ( (!_handler && _defaultHandlerResult) || (_handler && _handler(lazyFormattedMessage, message, flag, function, file, line)) ) {
+  if ( (!_handler && _defaultHandlerResult) || (_handler && _handler(lazyFormattedMessage, message, level, function, file, line)) ) {
     printf("%s\n", [lazyFormattedMessage() UTF8String]);
   }
 }
