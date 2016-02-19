70537b038c3a40d506a2098b6a54a679c449b81d
Roman Kulesha <kulesha.r@gmail.com>
2016-02-19 12:50:28 +0300

    fix error extension for swift

--- Source/Extensions/Foundation/NSError+OFExtension.h
+++ Source/Extensions/Foundation/NSError+OFExtension.h
@@ -4,8 +4,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface NSError (OFExtension)
 
-+ (instancetype)of_errorWithDomainObject:(id)domainObject description:(NSString *)description NS_SWIFT_NAME(init(of_domainObject:description:));
-+ (instancetype)of_errorWithDomainObject:(id)domainObject code:(NSInteger)code description:(NSString *)description NS_SWIFT_NAME(init(of_domainObject:code:description:));
++ (instancetype)of_errorWithDomainObject:(id)domainObject description:(NSString *)description NS_SWIFT_UNAVAILABLE("Use swift init instead");
++ (instancetype)of_errorWithDomainObject:(id)domainObject code:(NSInteger)code description:(NSString *)description NS_SWIFT_UNAVAILABLE("Use swift init instead");
 
 @end
 
