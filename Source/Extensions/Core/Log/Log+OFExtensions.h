fb30235271791ef65d596507bf501c99012ed35e
Roman Kulesha <kulesha.r@gmail.com>
2016-03-23 02:38:52 +0300

    fix log extension macro

--- Source/Extensions/Core/Log/Log+OFExtensions.h
+++ Source/Extensions/Core/Log/Log+OFExtensions.h
@@ -16,9 +16,14 @@ typedef BOOL (^OFLogHandler)(OFLogLazyMessage lazyFormattedMessage, NSString * _
 FOUNDATION_EXPORT void __OFLog(NSString * _Nullable message, OFLogLevel level, NSString *function, NSString *file, unsigned int line);
 FOUNDATION_EXPORT void OFLogSetup(OFLogHandler handler); // setup which message should print, by default print all in DEBUG and nothing otherwise
 
-#define OFLog(objectOrFormat, ...) __OFLog(objectOrFormat ? [NSString stringWithFormat:[objectOrFormat description],##__VA_ARGS__] : nil, OFLogLevelDefault, __func__, __FILE__, __LINE__)
-#define OFLogInfo(objectOrFormat, ...) __OFLog(objectOrFormat ? [NSString stringWithFormat:[objectOrFormat description],##__VA_ARGS__] : nil, OFLogLevelInfo, __func__, __FILE__, __LINE__)
-#define OFLogWarning(objectOrFormat,...) __OFLog(objectOrFormat ? [NSString stringWithFormat:[objectOrFormat description],##__VA_ARGS__] : nil, OFLogLevelWarning, __func__, __FILE__, __LINE__)
-#define OFLogError(objectOrFormat, ...) __OFLog(objectOrFormat ? [NSString stringWithFormat:[objectOrFormat description],##__VA_ARGS__] : nil, OFLogLevelError, __func__, __FILE__, __LINE__)
+#define OFLog(object) __OFLog([object description], OFLogLevelDefault, [NSString stringWithUTF8String:__func__], [NSString stringWithUTF8String:__FILE__], __LINE__)
+#define OFLogInfo(object) __OFLog([object description], OFLogLevelInfo, [NSString stringWithUTF8String:__func__], [NSString stringWithUTF8String:__FILE__], __LINE__)
+#define OFLogWarning(object) __OFLog([object description], OFLogLevelWarning, [NSString stringWithUTF8String:__func__], [NSString stringWithUTF8String:__FILE__], __LINE__)
+#define OFLogError(object) __OFLog([object description], OFLogLevelError, [NSString stringWithUTF8String:__func__], [NSString stringWithUTF8String:__FILE__], __LINE__)
+
+#define OFLogF(format, ...) __OFLog([NSString stringWithFormat:format,##__VA_ARGS__], OFLogLevelDefault, [NSString stringWithUTF8String:__func__], [NSString stringWithUTF8String:__FILE__], __LINE__)
+#define OFLogInfoF(format, ...) __OFLog([NSString stringWithFormat:format,##__VA_ARGS__], OFLogLevelInfo, [NSString stringWithUTF8String:__func__], [NSString stringWithUTF8String:__FILE__], __LINE__)
+#define OFLogWarningF(format, ...) __OFLog([NSString stringWithFormat:format,##__VA_ARGS__], OFLogLevelWarning, [NSString stringWithUTF8String:__func__], [NSString stringWithUTF8String:__FILE__], __LINE__)
+#define OFLogErrorF(format, ...) __OFLog([NSString stringWithFormat:format,##__VA_ARGS__], OFLogLevelError, [NSString stringWithUTF8String:__func__], [NSString stringWithUTF8String:__FILE__], __LINE__)
 
 NS_ASSUME_NONNULL_END
